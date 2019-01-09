042e2d8be673a73215e71a41f4851e29f6b5115d
Paul Drury <paul@drurys.org>
2019-01-09 10:35:23 +0000

    Added config validation with tetsts

---
+++ lib/src/config-validation.d.ts
@@ -0,0 +1,6 @@
+export declare class ConfigValidation {
+    static getBoolean(val: any, message: string, defaultValue?: boolean): boolean;
+    static getString(val: any, message: string, defaultValue?: string): string;
+    static getNumber(val: any, message: string, defaultValue?: number): number;
+    static getDate(val: any, message: string, defaultValue?: Date): Date;
+}
