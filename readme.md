d5b149eb24eb57ba283eba6adccb66e14ff17f7e
Paul Drury <paul@drurys.org>
2019-01-16 08:29:28 +0000

    updated from changes in web-app and server

--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 # HC-COMMON package
-This is a heating controller for European style water+radiators style heating system.
+This is a heating controller for European style water+radiators heating system.
 I deploy it on a Raspberry Pi, but in theory it will work on any hardware/OS with file-mapped devices.
 
 This is a hobby project, you are welcome to use the code but it insn't intended for public use.
--- src/types.ts
+++ src/types.ts
@@ -1,78 +1,10 @@
-/**
- * The remaining interfaces are for immutable classes
- */
-export interface IProgram {
-    id: string;
-    name: string;
-    minHwTemp: number;
-    maxHwTemp: number;
-    getRules(): ReadonlyArray<IRule>;
-}
-
-export interface IRule {
-    startTime: ITimeOfDay;
-    endTime: ITimeOfDay;
-    applyRule(currentState: IControlState, readings: ReadonlyArray<IReading>, time: ITimeOfDay | Date): IRuleResult;
-}
-
-export interface IRuleResult {
-    heating: boolean | null;
-    hotWater: boolean | null;
-}
-
-export interface ITimeOfDay {
-    hour: number;
-    minute: number;
-    second: number;
-
-    isLaterThan(other: ITimeOfDay): boolean;
-    isSameAs(other: ITimeOfDay): boolean;
-    isEarlierThan(other: ITimeOfDay): boolean;
-
-    addHours(hours: number): ITimeOfDay;
-    addMinutes(minutes: number): ITimeOfDay;
-    addSeconds(seconds: number): ITimeOfDay;
-}
-
-export interface IControlState {
-    heating: boolean;
-    hotWater: boolean;
-}
-
-export interface IReading {
-    id: string;
-    description: string;
-    role: string;
-    value: number;
-}
-
-export interface IConfiguration {
-    getProgramConfig(): ReadonlyArray<IProgram>;
-    getNamedConfig(): INamedConfig;
-    getDatedConfig(): ReadonlyArray<IDatedConfig>;
-    getSensorConfig(): ReadonlyArray<ISensorConfig>;
-}
-
-export interface INamedConfig {
-    weekdayProgramId: string;
-    saturdayProgramId: string;
-    sundayProgramId: string;
-}
-
-export interface IDatedConfig {
-    programId: string;
-    date: Date;
-}
-
-export interface ISensorConfig {
-    id: string;
-    description: string;
-    role: string;
-    deleted: boolean;
-}
-
-export interface IOverride {
-    readonly id: string;
-    readonly date: Date;
-    readonly rule: IRule;
-}
+export { ConfigValidation } from "./config-validation";
+export * from "./configuration/basic-heating-rule";
+export * from "./configuration/configuration";
+export * from "./configuration/dated-config";
+export * from "./configuration/named-config";
+export * from "./configuration/override";
+export * from "./configuration/program";
+export * from "./configuration/sensor-config";
+export * from "./configuration/time-of-day";
+export * from "./interfaces";
