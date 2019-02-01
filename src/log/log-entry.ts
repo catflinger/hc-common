62d65652ad9ecfc14c7871bba71260ffa5074cae
Paul Drury <paul@drurys.org>
2019-02-01 18:08:11 +0000

    added classes for log entities

---
+++ src/log/log-entry.ts
@@ -0,0 +1,20 @@
+import { ConfigValidation } from "../config-validation";
+import { ILogEntry } from "../interfaces";
+
+export class LogEntry implements ILogEntry {
+    public readonly date: Date;
+    public readonly heating: boolean;
+    public readonly hotWater: boolean;
+    public readonly readings: ReadonlyArray<number>;
+
+    constructor(data: any) {
+        this.date = ConfigValidation.getDate(data.date, "LogEntry.date");
+        this.heating = ConfigValidation.getBoolean(data.heating, "LogEntry.heating");
+        this.hotWater = ConfigValidation.getBoolean(data.hotWater, "LogEntry.hotWater");
+        const readings = [];
+
+        data.readings.forEach((reading: any) => {
+            readings.push(ConfigValidation.getNumber(reading, "LogEntry.readings"));
+        });
+    }
+}
---
+++ test/log/log-spec.ts
@@ -0,0 +1,82 @@
+import "mocha";
+import * as chai from "chai";
+
+import { LogExtract } from "../../src/log/log-extract";
+
+const expect = chai.expect;
+
+describe("LogExtract", () => {
+
+    describe("loading with valid data", () => {
+
+        it ("should load with minimal data", () => {
+            let extract = new LogExtract(getMinimalData()); 
+        });
+
+        it ("should load with full data", () => {
+            let extract = new LogExtract(fullData); 
+        });
+
+        it ("should throw with no data supplied", () => {
+            expect(() => {
+                new LogExtract({}); 
+            }).to.throw;
+        });
+
+        it ("should throw with bad from", () => {
+            let md = getMinimalData();
+            md.from = "boo hoo";
+            expect(() => new LogExtract(md)).to.throw; 
+        });
+
+        it ("should throw with bad from", () => {
+            let md = getMinimalData();
+            md.to = "bad boy!";
+            expect(() => new LogExtract(md)).to.throw; 
+        });
+
+        it ("should throw with missing sensors", () => {
+            let md = getMinimalData();
+            md.sensors = undefined;
+            expect(() => new LogExtract(md)).to.throw; 
+        });
+
+        it ("should throw with missing entries", () => {
+            let md = getMinimalData();
+            md.entries = undefined;
+            expect(() => new LogExtract(md)).to.throw; 
+        });
+
+    });
+});
+
+function getMinimalData(): any {
+    return Object.assign({}, minimalData);
+}
+
+const minimalData: any = {
+    from: "2019-12-13T12:00:00",
+    to: "2019-12-13T12:11:00",
+    sensors: [],
+    entries: [],
+}
+
+const fullData: any = {
+    from: "2019-12-13T12:00:00",
+    to: "2019-12-13T12:11:00",
+    sensors: ["12", "13"],
+    entries: [ 
+        { 
+            date: "2019-12-13T12:00:00",
+            hotWater: true,
+            heating: false,
+            readings: [ 11.1, 12.2],
+        },
+        { 
+            date: "2019-12-13T12:10:00",
+            hotWater: false,
+            heating: true,
+            readings: [ 11.3, 12.0],
+        },
+    ],
+}
